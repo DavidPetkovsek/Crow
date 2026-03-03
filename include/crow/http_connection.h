6f3b6c6445ecff08859e3809e5124f6a894ba012
DavidPetkovsek <david.petkovsek@martinrea.com>
2026-03-03 10:47:51 -0500

    Update other options checks to include new catch_all check

--- include/crow/http_connection.h
+++ include/crow/http_connection.h
@@ -142,7 +142,7 @@ namespace crow
                 need_to_call_after_handlers_ = true;
                 complete_request();
             }
-            if (!routing_handle_result_->rule_index && req_.method == HTTPMethod::Options)
+            if (!routing_handle_result_->rule_index && !routing_handle_result_->catch_all && req_.method == HTTPMethod::Options)
             {
                 parser_.done();
                 need_to_call_after_handlers_ = true;
